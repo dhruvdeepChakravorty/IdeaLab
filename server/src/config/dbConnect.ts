5ec3c0b9ca23961ff0b58af1d5be0aa92975e268
Dhruvdeep Chakravorty <dhruvdeepchakravorty17@gmail.com>
2026-03-26 15:02:36 +0530

    fix: Added return satement if DB already connected

--- server/src/config/dbConnect.ts
+++ server/src/config/dbConnect.ts
@@ -1,26 +1,26 @@
 import mongoose from "mongoose";
 
 const dbConnect = async () => {
+  if (mongoose.connection.readyState == 1) {
+    console.log("Server already connected");
+    return;
+  }
+  const uri = process.env.MONGO_URI;
 
-     if (mongoose.connection.readyState==1) {
-            console.log('Server already connected')
-        }
-    const uri = process.env.MONGO_URI
+  if (!uri) {
+    console.log("URI doesnt exist in ENV");
+    process.exit(1);
+  }
 
-    if(!uri){
-        console.log("URI doesnt exist in ENV")
-        process.exit(1)
+  try {
+    await mongoose.connect(uri);
+    console.log(`connection successful ${mongoose.connection.host}`);
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.log(`Connection Failed: ${error.message} `);
     }
-    
-    try {
-        await mongoose.connect(uri)
-       console.log(`connection successful ${mongoose.connection.host}`)
-    } catch (error:unknown) {
-         if (error instanceof Error) {
-            console.log(`Connection Failed: ${error.message} `)
-         }
-         process.exit(1)
-    }
-}
+    process.exit(1);
+  }
+};
 
-export default dbConnect
+export default dbConnect;
